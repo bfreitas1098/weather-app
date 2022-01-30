49cb8667f07c5c5e936c59d4fba0a41d9f75c313
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-01-30 15:59:37 -0500

    Integrated the API into JS to get accurate forecast data

--- src/index.js
+++ src/index.js
@@ -144,29 +144,41 @@ fahrenheitButton.addEventListener("click", displayFahrenheitTemperatures);
 
 // Functions to display the forecast
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-        <div class="forecast-day">${day}</div>
+  forecast.forEach(function (forecastDay, index) {
+    if (index > 0 && index <= 5) {
+      let forecastMax = Math.round(forecastDay.temp.max);
+      let forecastMin = Math.round(forecastDay.temp.min);
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+        <div class="forecast-day">${formatDay(forecastDay.dt)}</div>
         <img
           id="icon"
-          src="https://openweathermap.org/img/wn/04d@2x.png"
+          src="https://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt="clouds"
           width="50px"
         />
         <br />
         <div class="forecast-temperatures">
-          <span class="forecast-weather-max">75° </span>
-          <span class="forecast-weather-min"> 60°</span>
+          <span class="forecast-weather-max">${forecastMax}°</span>
+          <span class="forecast-weather-min"> ${forecastMin}°</span>
         </div>
       </div>
   `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
